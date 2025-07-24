b2991c4595fa6e7f72865c32bc41f636e1149f85
shelnuttnq <shelnuttnq@appstate.edu>
2025-07-24 11:35:49 -0400

    added inner class VarInfo to hold variable information

--- bad_names_plugin/src/main/java/edu/appstate/cs/checker/BadLocalVarChecker.java
+++ bad_names_plugin/src/main/java/edu/appstate/cs/checker/BadLocalVarChecker.java
@@ -10,10 +10,80 @@ import com.sun.source.util.TreeScanner;
 
 import java.util.*;
 
+import javax.lang.model.element.Name;
+
 import static com.google.errorprone.BugPattern.LinkType.CUSTOM;
 import static com.google.errorprone.BugPattern.SeverityLevel.WARNING;
 
-public class BadLocalVarChecker 
+@AutoService(BugChecker.class)
+@BugPattern(
+        name = "BadLocalVarChecker",
+        summary = "Detects unnecessary local variables that are immediately returned or only used once",
+        severity = WARNING,
+        linkType = CUSTOM,
+        link = "https://github.com/nicholasshelnutt/error-prone-lab"
+)
+public class BadLocalVarChecker extends BugChecker implements 
+    BugChecker.MethodTreeMatcher
 {
-    
+    // holds var info
+    private static class VarInfo
+    {
+        private final VariableTree dtree;
+        private int usageCount = 0;
+        private boolean reassigned = false;
+        private boolean immediatlyReturned = false;
+        private boolean usedInLoop = false;
+
+        public VarInfo(VariableTree dtree) 
+        {
+            this.dtree = dtree;
+        }
+
+        public VariableTree getDtree() 
+        {
+            return dtree;
+        }
+
+        public int getUsageCount() 
+        {
+            return usageCount;
+        }
+
+        public void incrementUsageCount() 
+        {
+            usageCount++;
+        }
+
+        public boolean isReassigned() 
+        {
+            return reassigned;
+        }
+
+        public void setReassign(boolean reassigned)
+        {
+            this.reassigned = reassigned;
+        }
+
+        public boolean isImmediatlyReturned() 
+        {
+            return immediatlyReturned;
+        }
+
+        public void setImmediatelyReturned(boolean immediatlyReturned) 
+        {
+            this.immediatlyReturned = immediatlyReturned;
+        }
+
+        public boolean isUsedInLoop() 
+        {
+            return usedInLoop;
+        }
+
+        public void setUsedInLoop(boolean usedInLoop) 
+        {
+            this.usedInLoop = usedInLoop;
+        }
+
+    }
 }
